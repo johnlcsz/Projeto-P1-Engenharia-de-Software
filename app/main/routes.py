9a33d3aaca1c48812a962b85b7e80a98fe62fb07
Jonathan <jlrn5426@gmail.com>
2026-06-02 21:03:53 -0300

    feat: Adiciona conteúdo do base.html e estrutura inicial de rotas

---
+++ app/main/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template, redirect, url_for, session
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+
+    if session.get('usuario'):
+        return redirect(url_for('usuario.pg-principal'))
+
+    return render_template('index.html')
